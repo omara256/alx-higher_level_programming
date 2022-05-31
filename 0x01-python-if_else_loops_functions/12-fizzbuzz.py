1a7edf183f3b9d1e342a705221be3861155c9a8d
omara256 <omaraperker56@gamil.com>
2022-05-31 06:21:53 -0700

    add: 12 Numbers 1 - 100

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
+
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 is 0 and i % 5 is 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 is 0:
-            print("Fizz", end=" ")
-        elif i % 5 is 0:
-            print("Buzz", end=" ")
-        else:
-            print("{:d}".format(i), end=" ")
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 != 0 and i % 5 != 0:
+            print("{:d}".format(i), end="")
+        print(" ", end="")
