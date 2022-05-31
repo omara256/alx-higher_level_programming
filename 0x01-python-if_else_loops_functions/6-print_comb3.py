775edd205a69f27d0148a5efbadfc19f88eecdec
omara256 <omaraperker56@gamil.com>
2022-05-31 06:06:47 -0700

    Task 6 Update Brains n Materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i is not 89 and i / 10 < i % 10:
-        print("{:02d}".format(i), end=", ")
-    elif i is 89:
-        print("{:02d}".format(i))
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
+            break
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
