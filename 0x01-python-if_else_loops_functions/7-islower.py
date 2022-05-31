d07f294d0c67b467cfa0ddbe24ea35ce1332f23a
omara256 <omaraperker56@gamil.com>
2022-05-31 06:09:39 -0700

    Task 7 Checks Lowercase Char

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
+
 def islower(c):
-    return ord(c) in range(ord('a'), ord('z') + 1)
+    ch = ord(c)
+    if ch >= 97 and ch <= 122:
+        return True
+    else:
+        return False
