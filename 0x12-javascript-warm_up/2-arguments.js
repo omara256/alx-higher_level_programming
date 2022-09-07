21d2ecd3574d98924149ccfc8e1f6ee96e7d5890
omara256 <omaraperker56@gamil.com>
2022-09-07 01:16:57 -0700

    Added Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2;
-if (numArgs === 0) {
+
+/* Prints a message depending on number of aruments passed */
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (numArgs === 1) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
