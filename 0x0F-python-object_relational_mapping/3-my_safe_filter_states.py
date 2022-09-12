ebc83f1be61b66089ff410a084590758a9f59979
omara256 <omaraperker56@gamil.com>
2022-09-12 05:06:42 -0700

    Task 3 Added

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""3-my_safe_filter_states
-Takes in arguments and displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument. Safe from MySQL injections!
+"""
+a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+and is safe from SQL injections
 """
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+import MySQLdb
+from sys import argv
 
-    with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         db=argv[3], port=3306) as db:
-        db.execute("SELECT *\
-                   FROM states\
-                   WHERE name = %s", (argv[4],))
-        table = db.fetchall()
-        for data in table:
-            print(data)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", 
+                   (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
